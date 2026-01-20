e8b0a39c41e98363208f99b32ce735ceb76f5f61
Andre <andrepvgabriel@gmail.com>
2026-01-20 15:38:51 -0300

    Tiktok Pixel

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -42,6 +42,19 @@ export function TrackingScripts() {
   (function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:${hotjarId},hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 })();`;
 
+  // TikTok Pixel - placed in <head> and avoided on /admin (same behavior as GA/Hotjar).
+  const tiktokPixelId = (process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID ?? 'D5NSLQBC77U0089IST1G').trim();
+  const tiktokSnippet = `(function(){
+  try { if (window.location && window.location.pathname && window.location.pathname.indexOf('/admin') === 0) { return; } } catch (e) {}
+  !function (w, d, t) {
+    w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+  var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+  ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+    ttq.load('${tiktokPixelId}');
+    ttq.page();
+  }(window, document, 'ttq');
+})();`;
+
   return (
     <>
       <Script id="google-gtag" strategy="beforeInteractive">
@@ -49,6 +62,9 @@ export function TrackingScripts() {
       </Script>
       <Script id="hotjar" strategy="beforeInteractive">
         {hotjarSnippet}
+      </Script>
+      <Script id="tiktok-pixel" strategy="beforeInteractive">
+        {tiktokSnippet}
       </Script>
     </>
   );
